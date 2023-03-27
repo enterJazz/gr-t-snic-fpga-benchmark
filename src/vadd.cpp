43b83d4694422c78f76f359a1dac8881e0de7fc9
Robert Schambach <scha@in.tum.de>
2023-03-27 10:36:50 +0200

    Add init src code

---
+++ src/vadd.cpp
@@ -0,0 +1,19 @@
+extern "C" {
+	int counter = 0;
+	void attest(
+	        const unsigned int *in, // Read-Only Vector
+	        unsigned int *out,       // Output Result
+	        int size                 // Size in integer
+	        )
+	{
+#pragma HLS INTERFACE m_axi port=in bundle=aximm1
+#pragma HLS INTERFACE m_axi port=out bundle=aximm1
+
+	    for(int i = 0; i < size; ++i)
+	    {
+		// TODO: implement
+	        // out[i] = in1[i] + in2[i];
+	    }
+	}
+}
+
