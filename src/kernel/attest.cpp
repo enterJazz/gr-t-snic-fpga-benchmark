678b021ef4267a4cd43a29a589b49a099ae2de17
Robert Schambach <scha@in.tum.de>
2023-04-10 19:03:39 +0200

    wip: populate kernel code

--- src/kernel/attest.cpp
+++ src/kernel/attest.cpp
@@ -1,15 +1,12 @@
-#include <stdint.h>
+#include <stdint.h> // uint8_t, uint32_t
 
 extern "C" {
 #include "util.h"
-#include <hmac-sha256.h>
 }
 
 
-//TODO: construct `data`: append msg to counter ?
-//TODO: use differnt type for counter, as int size is compiler dependent
 extern "C" {
-	int counter = 0;
+	uint32_t counter = 0;
 	uint8_t myKey[KEY_LEN] = { 0x0 };
 	void attest(
 	        uint8_t *in_msg_hash,	// Read-Only Vector
@@ -18,67 +15,21 @@ extern "C" {
 	{
 #pragma HLS INTERFACE m_axi port=in_msg_hash bundle=aximm1
 #pragma HLS INTERFACE m_axi port=out_attestation bundle=aximm1
-
-	// populate input w/ zeros
-	uint8_t in_data[INPUT_LEN] = { 0x0 };
-	// stores counter to bytes conversion
-	uint8_t counter_bytes[COUNTER_LEN];
-
-	// prepare hash input
-	// populate input w/ msg_hash
-	for (int i = 0 ; i < INPUT_MSG_HASH_LEN ; i++ ) {
-		in_data[i] = in_msg_hash[i];
-	}
-
-	// populate input w/ counter (as bytes)
-	// transform counter to byte array via shifts
-	counter_bytes[0] = (counter >> 24) & 0xFF;
-	counter_bytes[1] = (counter >> 16) & 0xFF;
-	counter_bytes[2] = (counter >> 8) & 0xFF;
-	counter_bytes[3] = counter & 0xFF;
-
-	for (int i = 0 ; i < COUNTER_LEN ; i++ ) {
-		in_data[INPUT_MSG_HASH_LEN + i] = counter_bytes[i];
-	}
-
-	// increment counter ; NOTE differs from trinc
-	counter++;
-
-	// h  = hash({key, val, counter_val});
-	// attestation = encrypt(h);
-	// return attestation
-
-
-	//hmac_sha256 (uint8_t out[HMAC_SHA256_DIGEST_SIZE],
-	//             const uint8_t *data, size_t data_len,
-	//             const uint8_t *key, size_t key_len);
-	//
-	//             out = out_attestation : binding hash of key to message hash and counter value
-	//             data = message hash || counter val
-	//             key = key
-	//
-	//             TODO: what is sent to other party? message, hmac, counter val?
-
-	// TODO: is hmac the correct operation? why does Dimitra use encryption?
-	// TODO: asymmetric or symetric crypto? HMAC: symetric -> implement for now
-	// due to simplicity
-	// NOTE: out attestation MUST have size HMAC_SHA256_DIGEST_SIZE (32)
-
-	hmac_sha256(
+	
+	compute_msg_hmac(
 		out_attestation,
-		in_data,
-		INPUT_LEN,
-		myKey,
-		KEY_LEN
+		in_msg_hash,
+		counter,
+		myKey
 	);
 
-	
 	}
 }
 
 
 #ifdef _DEBUG_KERNEL_BUILD
-#include <stdio.h>
+#include <stdio.h> // printf
+#include <hmac-sha256.h> // HMAC_SHA256_DIGEST_SIZE
 int
 main() {	
 	uint8_t in_data[INPUT_MSG_HASH_LEN] = { 0xFF };
--- src/kernel/verify.cpp
+++ src/kernel/verify.cpp
@@ -1,86 +1,44 @@
-#include <stdint.h>
+#include <stdint.h> // size_t, uint8_t, uint32_t
 
 extern "C" {
-#include <hmac-sha256.h>
+#include "util.h" // compute_msg_hmac
+#include <hmac-sha256.h> // HMAC_SHA256_DIGEST_SIZE
+#include <stdbool.h>
 }
 
-// https://crypto.stackexchange.com/a/34866
-#define KEY_LEN 32
-// input len = message hash len (256 bit -> 32 byte) + counter max size len (log(int) = 4)
-// NOTE: int max len differs per compiler (see https://stackoverflow.com/questions/11438794/is-the-size-of-c-int-2-bytes-or-4-bytes)
-#define INPUT_MSG_HASH_LEN 32
-#define INT_MAX_LEN 4
-#define INPUT_LEN (INPUT_MSG_HASH_LEN + INT_MAX_LEN)
 
-//TODO: construct `data`: append msg to counter ?
-//TODO: use differnt type for counter, as int size is compiler dependent
 extern "C" {
-	int counter = 0;
+	uint32_t counter = 0;
 	uint8_t myKey[KEY_LEN] = { 0x0 };
 	void verify(
 	        uint8_t *in_msg_hash,	// Read-Only Vector
-	        uint8_t *in_msg,  	// Read-Only Vector
-	        uint8_t *out_verify_result  	// Output Result
+	        uint8_t *in_msg_hmac,  	// Read-Only Vector
+	        bool out_verify_result  	// Output Result
 	        )
 	{
 #pragma HLS INTERFACE m_axi port=in_msg_hash bundle=aximm1
 #pragma HLS INTERFACE m_axi port=in_msg bundle=aximm1
 #pragma HLS INTERFACE m_axi port=out_verify_result bundle=aximm1
 
-	// populate input w/ zeros
-	uint8_t in_data[INPUT_LEN] = { 0x0 };
-	// stores counter to bytes conversion
-	uint8_t counter_bytes[INT_MAX_LEN];
+	uint8_t own_msg_hmac[HMAC_SHA256_DIGEST_SIZE];
 
-	// prepare hash input
-	// populate input w/ msg_hash
-	for (int i = 0 ; i < INPUT_MSG_HASH_LEN ; i++ ) {
-		in_data[i] = in_msg_hash[i];
-	}
-
-	// populate input w/ counter (as bytes)
-	// transform counter to byte array via shifts
-	counter_bytes[0] = (counter >> 24) & 0xFF;
-	counter_bytes[1] = (counter >> 16) & 0xFF;
-	counter_bytes[2] = (counter >> 8) & 0xFF;
-	counter_bytes[3] = counter & 0xFF;
-
-	for (int i = 0 ; i < INT_MAX_LEN ; i++ ) {
-		in_data[INPUT_MSG_HASH_LEN + i] = counter_bytes[i];
-	}
-
-	// increment counter ; NOTE differs from trinc
-	counter++;
-
-	// h  = hash({key, val, counter_val});
-	// attestation = encrypt(h);
-	// return attestation
-
-
-	//hmac_sha256 (uint8_t out[HMAC_SHA256_DIGEST_SIZE],
-	//             const uint8_t *data, size_t data_len,
-	//             const uint8_t *key, size_t key_len);
-	//
-	//             out = out_attestation : binding hash of key to message hash and counter value
-	//             data = message hash || counter val
-	//             key = key
-	//
-	//             TODO: what is sent to other party? message, hmac, counter val?
-
-	// TODO: is hmac the correct operation? why does Dimitra use encryption?
-	// TODO: asymmetric or symetric crypto? HMAC: symetric -> implement for now
-	// due to simplicity
-	// NOTE: out attestation MUST have size HMAC_SHA256_DIGEST_SIZE (32)
-
-	hmac_sha256(
-		out_attestation,
-		in_data,
-		INPUT_LEN,
-		myKey,
-		KEY_LEN
+	compute_msg_hmac(
+		own_msg_hmac,
+		in_msg_hash,
+		counter,
+		myKey
 	);
 
-	
+	// compare hash results
+	for (size_t i = 0 ; i < HMAC_SHA256_DIGEST_SIZE ; i++) {
+		if (in_msg_hmac[i] != own_msg_hmac[i]) {
+			out_verify_result = false;
+			return;
+		}
+	}
+
+	out_verify_result = true;
+
 	}
 }
 
@@ -90,16 +48,19 @@ extern "C" {
 int
 main() {	
 	uint8_t in_data[INPUT_MSG_HASH_LEN] = { 0xFF };
-	uint8_t out_data[HMAC_SHA256_DIGEST_SIZE] = { 0x00 };
-	attest(
+	uint8_t in_msg_hmac[HMAC_SHA256_DIGEST_SIZE] = { 0x00 };
+	bool out_result;
+	verify(
 		in_data,
-		out_data
+		in_msg_hmac,
+		out_result
 	);
 	
-	for (int i = 0; i < HMAC_SHA256_DIGEST_SIZE ; i++) {
-		printf("%x", out_data[i]);
+	if (out_result) {
+		printf("HMACs match\n");
+	} else {
+		printf("HMACs DO NOT match\n");
 	}
-	printf("\n");
 }
 #endif
 
