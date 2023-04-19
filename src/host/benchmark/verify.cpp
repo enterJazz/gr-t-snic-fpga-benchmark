bc4c2582a0eff1c565b726ae1c4cd975d91f6c88
Robert Schambach <scha@in.tum.de>
2023-04-19 07:45:24 +0200

    milestone: select between kernels

--- src/host/benchmark/verify.cpp
+++ src/host/benchmark/verify.cpp
@@ -28,38 +28,40 @@ namespace benchmark::verify
         //
         // input buffer
         // Match kernel arguments to RTL kernel
-        auto bo_in_msg_hash = xrt::bo(
-                device, input_msg_hash_size, in_krnl.group_id(device_input_group_id)
+        auto bo_in = xrt::bo(
+                device,
+                (input_msg_hash_size + output_attestation_hash_size),
+                in_krnl.group_id(device_input_group_id)
         );
-        // input buffer
-        auto bo_in_attestation_hmac = xrt::bo(
-                device, output_attestation_hash_size, in_krnl.group_id(device_input_group_id)
-        );
+
         // output buffer
         auto bo_out = xrt::bo(
-                device, output_attestation_hash_size, in_krnl.group_id(device_output_group_id)
+            device,
+            output_attestation_hash_size,
+            in_krnl.group_id(device_output_group_id)
         );
 
         // Map the contents of the buffer object into host memory
         // NOTE: message contents in here
-        auto bo_in_hash_map = bo_in_msg_hash.map<uint8_t*>();
-        auto bo_in_hmac_map = bo_in_attestation_hmac.map<uint8_t*>();
+        auto bo_in_map = bo_in.map<uint8_t*>();
         auto bo_out_map = bo_out.map<uint8_t*>();
-        std::fill(bo_in_hash_map, bo_in_hash_map + input_msg_hash_size, 0);
-        std::fill(bo_in_hmac_map, bo_in_hmac_map + output_attestation_hash_size, 0);
+        std::fill(
+            bo_in_map,
+            bo_in_map + (input_msg_hash_size + output_attestation_hash_size),
+            0
+        );
+
         std::fill(bo_out_map, bo_out_map + output_attestation_hash_size, 0);
 
-        utils::populate_input_data(bo_in_hash_map, input_msg_hash_size);
-        utils::populate_input_data(bo_in_hmac_map, output_attestation_hash_size);
+        utils::populate_input_data(bo_in_map, (input_msg_hash_size + output_attestation_hash_size));
 
         // synchronize input buffer data to device global memory
-        bo_in_msg_hash.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-        bo_in_attestation_hmac.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+        bo_in.sync(XCL_BO_SYNC_BO_TO_DEVICE);
 
         // Execution of the kernel
         // delay run to benchmark function
         // see https://xilinx.github.io/XRT/2022.1/html/xrt_native_apis.html#other-kernel-apis
-        auto run = in_krnl(bo_in_msg_hash, bo_in_attestation_hmac, bo_out);
+        auto run = in_krnl(bo_in, bo_out);
         // bring run into stopped state
         run.wait();
 
